78ae03bc73eb28c099befe4454dcf8761ee8049c
jihanfarah <jfarahs@gmail.com>
2025-03-27 17:42:11 +0800

    update code

--- key-definitions/homescreen_key.js
+++ key-definitions/homescreen_key.js
@@ -1,4 +1,5 @@
 class homescreenKey{
-    TITLE = `id`
+    TITLE = `//div[@class="app_logo" and text()="Swag Labs"]`
+    CHECKOUT_BTN = '#shopping_cart_container'
 }
 module.exports() = new homescreenKey()
